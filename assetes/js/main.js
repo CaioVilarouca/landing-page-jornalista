6f69282e29995afd63c58f57f03a78b9a4887ffa
Caio <caiovilarouca@gmail.com>
2026-06-16 13:27:41 -0300

    add background header

--- assetes/js/main.js
+++ assetes/js/main.js
@@ -16,3 +16,10 @@ if (navClose) {
         navMenu.classList.remove('show-menu')
     })
 }
+
+/* ========== CHANGE BACKGROUND HEADER ========== */
+const bgHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header'):header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
